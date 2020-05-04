25c3d8ff6a7308e879443ff5a5eec7adf39e089b
Annada Jena <annadasankarjena@gmail.com>
2020-05-04 15:14:12 +0530

    test

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <div>My Profile</div>
+      <div>My Profile updateds</div>
     );
   }
 }
